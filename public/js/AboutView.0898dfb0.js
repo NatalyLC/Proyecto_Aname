72807aff6a1d5df34555c85de2ce3fc6e19859f2
NatalyLc <natalymlc@hotmail.com>
2024-01-05 13:40:59 -0500

    first commit

---
+++ public/js/AboutView.0898dfb0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcloud"]=self["webpackChunkcloud"]||[]).push([[678],{833:function(n,u,t){t.r(u),t.d(u,{default:function(){return d}});var a=t(3396);const c={class:"about"},e=(0,a._)("h1",null,"This is an about page",-1),r=[e];function s(n,u){return(0,a.wg)(),(0,a.iD)("div",c,r)}var o=t(89);const i={},l=(0,o.Z)(i,[["render",s]]);var d=l}}]);
+//# sourceMappingURL=AboutView.0898dfb0.js.map
